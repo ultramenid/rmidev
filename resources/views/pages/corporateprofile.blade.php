c9b5e690110378ce72189b5b163287eab5e8d602
ultramenid <malichamdan@gmail.com>
2021-07-27 18:17:43 +0700

    make it better

--- resources/views/pages/corporateprofile.blade.php
+++ resources/views/pages/corporateprofile.blade.php
@@ -20,7 +20,7 @@
                 </div>
 
                 <h1 class="uppercase text-gray-800 text-3xl sm:text-left text-center font-bold py-6 px-6">Corporate Profile</h1>
-                <p class="text-gray-800 text-xl sm:text-left text-justify py-6 px-6">
+                <p class="text-gray-800 text-xl text-left py-6 px-6">
                     The RMI Reports 2020 covers 38 companies from 19 home countries, including publicly-listed, stated-owned and private companies. These companies operate in more than 780 mine sites in 49 producing countries and control over 200 closed or suspended mine sites. The assesment covers most mined commodities, excluding oil and gas. it is possible to filter by company, using the list of all companies on the left hand side, and also to filter by producing country or commodity, using the fields below.
                 </p>
             </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-black py-10 shadow-2xl">
+<div class="bg-black py-10 shadow-2xl sm:block hidden">
     <div class="max-w-6xl mx-auto px-6 md:px-8 bg-black">
         <nav class="flex lg:space-x-52 md:space-x-20 space-x-5 leading-5 overflow-x-auto scrollbar-hide ">
             <a href="{{ url('/about') }}" class="text-gray-300 uppercase whitespace-nowrap font-semibold hover:underline ">ABOUT</a>
