86abd7f620011172b036fb8a6d0ba5ddd85ee864
ultramenid <malichamdan@gmail.com>
2021-07-27 18:21:34 +0700

    make it better

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,18 @@
 <header class="bg-black">
-    <div x-data="{ open: false }" class="px-3 py-2 bg-black fixed top-0 z-10 sm:hidden block">
+    <div x-data="{ open: false }" class="px-6 py-4 bg-black fixed top-0 z-10 sm:hidden block">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor" @click="open = true">
             <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
           </svg>
 
-        <div class="fixed w-2/3 h-screen z-50 bg-black  inset-0 overflow-y-auto " x-show="open" @click.outside="open = false" x-cloak style="display: none !important">
+        <div class="fixed w-2/3 h-screen z-50 bg-black  inset-0 overflow-y-auto " x-show="open"
+        x-transition:enter="transition ease-in-out duration-150"
+        x-transition:enter-start="opacity-0"
+        x-transition:enter-end="opacity-100"
+        x-transition:leave="transition ease-in-out duration-150"
+        x-transition:leave-start="opacity-100"
+        x-transition:leave-end="opacity-0"
+        @click.outside="open = false"
+        x-cloak style="display: none !important">
             <button class="absolute top-4 left-6 focus:outline-none text-gray-300" @click="open = false">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="white" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
