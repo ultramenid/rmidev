568156f88bc714439ffb5c089e9ea940038f91b0
ultramenid <malichamdan@gmail.com>
2021-08-01 00:59:57 +0700

    make it better

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -10,7 +10,7 @@
     @yield('meta')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" defer>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <script src="{{ asset('js/app.js') }}" defer data-turbolinks-suppress-warning></script>
+    <script src="{{ asset('js/app.js') }}" defer ></script>
 
     @livewireStyles
 </head>
