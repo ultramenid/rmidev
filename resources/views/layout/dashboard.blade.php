45a78beb83f0248c641d5b08334a8225829cb466
ultramenid <malichamdan@gmail.com>
2021-09-02 13:50:31 +0700

    make it better

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -8,7 +8,7 @@
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
-    <link rel="shortcut icon" href="{{ asset('assets/minecraft.png') }}" type="image/x-icon" >
+    <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon" >
     <title>{{$title}}</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="theme-color" content="black" />
-    <link rel="shortcut icon" href="{{ asset('assets/minecraft.png') }}" type="image/x-icon" >
+    <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon" >
     <title>{{$title}}</title>
     @yield('meta')
 
