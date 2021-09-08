f5d53176ca891472c2f1e46c2a8ecc4272253e5c
ultramenid <malichamdan@gmail.com>
2021-09-08 11:10:34 +0700

    make it better

--- resources/views/pages/cms/cmspartials/navbar.blade.php
+++ resources/views/pages/cms/cmspartials/navbar.blade.php
@@ -3,12 +3,12 @@
         <nav x-data="{ pages: false }" class="-mb-px flex space-x-6 text-sm leading-5 overflow-x-auto scrollbar-hide text-gray-500">
             <a href="{{url('/cms/dashboard')}}" class=" px-0.5 py-3  @if($nav == 'dashboard' )  dark:border-gray-300 border-newgray-900 text-newgray-900 border-b-2 dark:text-gray-300 @endif   hover:text-newgray-900 dark:hover:text-gray-300 cursor-pointer   " >Dashboard</a>
 
-            <a @click="pages = ! pages" class="px-0.5 py-3 @if($nav == 'pages' ) dark:border-gray-300 border-newgray-900 text-newgray-900 dark:text-gray-300 border-b-2  @endif   hover:text-newgray-900 dark:hover:text-gray-300 cursor-pointer inline-flex  items-center" >
+            {{-- <a @click="pages = ! pages" class="px-0.5 py-3 @if($nav == 'pages' ) dark:border-gray-300 border-newgray-900 text-newgray-900 dark:text-gray-300 border-b-2  @endif   hover:text-newgray-900 dark:hover:text-gray-300 cursor-pointer inline-flex  items-center" >
                 Pages
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-4 ml-1 -mb-1" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                   </svg>
-            </a>
+            </a> --}}
             <div @click.away="pages=false" x-show="pages" x-transition class="absolute w-52 mt-11 " x-cloak style="display: none !important">
                 <div class="flex-col flex bg-white dark:bg-black px-3 py-3 space-y-2 rounded-md shadow-lg">
                     <a class="hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-newgray-900 text-gray-700 h-6" href="{{ url('/cms/page/methodology') }}" >Methodogy</a>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/alpine.js', 'public/js')
+    .js('resources/js/dark.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ]);
