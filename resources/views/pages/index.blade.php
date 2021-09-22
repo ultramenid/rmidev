f980f3918e3f235e0f4209c2c52011ebb0d668d1
ultramenid <malichamdan@gmail.com>
2021-09-22 16:54:13 +0700

    make it better

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -239,7 +239,7 @@
          {{-- map --}}
         <div class="max-w-6xl mx-auto px-6 md:px-8 sm:py-8 py-8">
             {{-- <img loading="lazy" src="{{ asset('assets/indonesiamap.png') }}" alt=""> --}}
-            <div class="flourish-embed flourish-map" data-src="visualisation/7315041"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
+            <iframe src='https://flo.uri.sh/visualisation/7315041/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
         </div>
 
 
